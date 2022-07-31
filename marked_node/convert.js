650ec0ac6c6871e8defcf852df949fe80b760acb
Pikka2048 <0312pika0201@gmail.com>
2022-07-31 16:51:00 +0900

    Convert markdown by marked node plugin

---
+++ marked_node/convert.js
@@ -0,0 +1,21 @@
+const program = require("commander");
+const fs = require("fs");
+// markedモジュールをmarkedオブジェクトとしてインポートする
+const marked = require("marked");
+
+program.parse(process.argv);
+const filePath = program.args[0];
+
+fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        process.exit(1);
+        return;
+    }
+    // MarkdownファイルをHTML文字列に変換する
+    const html = marked.parse(file);
+    console.log(html);
+    fs.writeFile('../about.html', html,function(err, result) {
+        if(err) console.log('error', err);
+});
+});
